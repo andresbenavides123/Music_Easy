e8a54a914f5bfe3f803188073b9bd6eed05111f2
andresbenavides123 <andres.benavidesb@campusucc.edu.co>
2026-04-14 23:41:54 +0000

    ajustes

--- music_backend/src/main.ts
+++ music_backend/src/main.ts
@@ -3,8 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Abierto total para el afán
+  app.enableCors(); // Sin restricciones para que no falle
+  
   const port = process.env.PORT || 3000;
+  // '0.0.0.0' ES LO QUE QUITA EL ERROR 502
   await app.listen(port, '0.0.0.0');
 }
 bootstrap();
